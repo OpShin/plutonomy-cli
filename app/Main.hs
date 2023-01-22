bb9ff90bb4225d46e87ba139db01bcddc1d5e6e1
Niels Mündler <n.muendler@posteo.de>
2023-01-22 01:01:45 +0100

    Add support for complex builtin types

--- app/Main.hs
+++ app/Main.hs
@@ -2,31 +2,37 @@
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE PartialTypeSignatures #-}
 
 module Main where
 
 import Options.Applicative
 
-import Control.Exception(throwIO)
+import Control.Exception(throwIO, Exception)
+import Control.Monad(void)
 import Data.Function((&))
 import Plutonomy.UPLC(optimizeUPLCWith, statsUPLC)
 import Plutonomy.Optimize(defaultOptimizerOptions, aggressiveOptimizerOptions)
 import PlutusCore.DeBruijn(FreeVariableError)
+import PlutusCore.Error
+import PlutusCore.Quote (Quote, runQuote, runQuoteT)
 import System.IO(stderr,stdout)
 import Text.PrettyBy.Default(display)
 import UntypedPlutusCore.Core(Term(..), Program(..))
 import UntypedPlutusCore.DeBruijn(deBruijnTerm)
 
-import qualified Data.ByteString.Lazy as BL
+import qualified Data.Text.IO as BT
 import qualified Data.ByteString.Char8 as B8
 import qualified UntypedPlutusCore.Parser as UPLC
+
+instance Exception ParserErrorBundle
 
 main :: IO ()
 main = do
     customExecParser (prefs showHelpOnEmpty) commandParser >>= \case
         Optimize { input, mode } -> do
-            bytes <- BL.readFile input
-            pgrm  <- parseDeBruijnProgram bytes
+            text <- BT.readFile input
+            pgrm  <- parseDeBruijnProgram text
             let opts = case mode of
                     DefaultMode -> defaultOptimizerOptions
                     AggressiveMode -> aggressiveOptimizerOptions
@@ -34,10 +40,11 @@ main = do
             B8.hPutStrLn stderr (B8.pack (compare pgrm pgrm'))
             B8.hPutStrLn stdout (B8.pack (display pgrm'))
   where
-    parseDeBruijnProgram bytes = do
-        pgrm <- UPLC.parseProgram bytes & either throwIO (return . fmap (const ()))
-        term  <- deBruijnTerm @FreeVariableError (_progTerm pgrm) & either throwIO return
-        return pgrm { _progTerm = term }
+    parseDeBruijnProgram text = do
+        pgrm <- runQuoteT (UPLC.parseProgram @ParserErrorBundle text) & either throwIO return
+        pgrmM <- (return . void) pgrm
+        term  <- deBruijnTerm @FreeVariableError (_progTerm pgrmM) & either throwIO return
+        return pgrmM { _progTerm = term }
 
     percentRatio num den =
         1000 * (1 - (fromIntegral num / fromIntegral den))
